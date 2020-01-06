9e25a616f58074059642a2671704dfa4d2b30f4a
khatia <khatiaakhalkatsishvili@gmail.com>
2020-01-07 00:15:23 +0400

    last

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import React from 'react';
-import './App.css';
-import ComboBox from './ComboBox';
+import React from "react";
+import "./App.css";
+import ComboBox from "./ComboBox";
 
 function App() {
-	return (
-		<div className="App">
-			<div className="select-container">
-				<ComboBox
-					data={[
-						{ valueField: 1, textField: 'One' },
-						{ valueField: 2, textField: 'Two' },
-						{ valueField: 3, textField: 'Three' },
-						{ valueField: 4, textField: 'Four' },
-						{ valueField: 5, textField: 'Five' },
-						{ valueField: 6, textField: 'Six' },
-						{ valueField: 7, textField: 'Seven' },
-						{ valueField: 8, textField: 'Eight' },
-						{ valueField: 9, textField: 'Nine' },
-						{ valueField: 10, textField: 'Ten' }
-					]}
-					placeholder={'Type to Search...'}
-					onSelectedItemChange={(valueFiled, textField) => {
-						console.log(valueFiled, textField);
-					}}
-					valueField={2}
-					textField={'Two'}
-					className="values"
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div className="select-container">
+        <ComboBox
+          data={[
+            { valueField: 1, textField: "Georgia" },
+            { valueField: 2, textField: "France" },
+            { valueField: 3, textField: "Spain" },
+            { valueField: 4, textField: "Norway" },
+            { valueField: 5, textField: "Germany" },
+            { valueField: 6, textField: "Poland" },
+            { valueField: 7, textField: "Italy" },
+            { valueField: 8, textField: " United Kingdom*" },
+            { valueField: 9, textField: "Bulgaria" },
+            { valueField: 9, textField: " Ireland" },
+            { valueField: 9, textField: "Croatia" },
+            { valueField: 10, textField: "Serbia" }
+          ]}
+          placeholder={"Type to Search..."}
+          onSelectedItemChange={(valueFiled, textField) => {
+            console.log(valueFiled, textField);
+          }}
+          valueField={2}
+          textField={"Two"}
+          className="values"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
